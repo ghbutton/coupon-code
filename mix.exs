0c6c486f149563e24a314b0748b55cc000349096
Gary Button <garyhbutton@gmail.com>
2026-04-24 14:22:46 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule CouponCode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
     ]
   end
